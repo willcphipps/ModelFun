5fd464411ca0e57b28be0193c28a45a3193513fa
William Phipps <willcphipps@gmail.com>
2020-04-14 16:05:12 -0500

    adding functionality

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,24 +6,68 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ModelFun.Models;
 
-namespace ModelFun.Controllers
-{
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
+namespace ModelFun.Controllers {
+    public class HomeController : Controller {
+
+        public IActionResult Index () {
+            User NewUser = new User () {
+                FirstName = "Donnie",
+                LastName = "Darko"
+            };
+            return View (NewUser);
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
+        [HttpGet ("Names")]
+        public IActionResult Names () {
+            string[] names = new string[] {
+                "Sally",
+                "Billy",
+                "Joey",
+                "Moose"
+            };
+            return View (names);
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        [HttpGet ("String")]
+        public IActionResult String () {
+            string mystring = "Naught of myself I do. Thus will he think, he who holds the truth of truths... Always assured: This is the sense world plays with senses.";
+            return View ("String", mystring);
+        }
+
+        [HttpGet ("Nums")]
+        public IActionResult Nums () {
+            int[] numbers = new int[] {
+                1,
+                2,
+                3,
+                4,
+                5,
+            };
+            return View (numbers);
+        }
+
+        [HttpGet ("Users")]
+        public IActionResult Users () {
+
+            User One = new User () {
+                FirstName = "Ralph Waldo",
+                LastName = "Emerson"
+            };
+            User Two = new User () {
+                FirstName = "Terrance",
+                LastName = "Mckenna"
+            };
+            User Three = new User () {
+                FirstName = "Michael",
+                LastName = "Polland"
+            };
+
+            List<User> newUsers = new List<User> () {
+                One,
+                Two,
+                Three
+            };
+            return View (newUsers);
         }
     }
 }
---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+
+namespace ModelFun.Models {
+    public class User {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
